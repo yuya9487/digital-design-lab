4929035c9ee84e2fdf19d88b98c1b9aa04f65f6f
yuya9487 <131440862+yuya9487@users.noreply.github.com>
2023-04-21 16:13:13 +0800

    homework 0421
    第四組

---
+++ HW_weekB_0421.ino
@@ -0,0 +1,56 @@
+byte seven_seg_digits[10][7] = { { 0,0,0,0,0,0,1 },  // = 0
+                                 { 1,0,0,1,1,1,1 },  // = 1
+                                 { 0,0,1,0,0,1,0 },  // = 2
+                                 { 0,0,0,0,1,1,0 },  // = 3
+                                 { 1,0,0,1,1,0,0 },  // = 4
+                                 { 0,1,0,0,1,0,0 },  // = 5
+                                 { 0,1,0,0,0,0,0 },  // = 6
+                                 { 0,0,0,1,1,1,1 },  // = 7
+                                 { 0,0,0,0,0,0,0 },  // = 8
+                                 { 0,0,0,0,1,0,0 }   // = 9
+                             };
+
+const int buttonPin = 12;
+int buttonState = 0;
+int num = 0;
+
+void setup() {   
+  Serial.begin (9600);         
+  pinMode(2, OUTPUT);  
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+ pinMode(9, OUTPUT);
+ 
+  digitalWrite(9, 1);  // 關閉小數點
+  digitalWrite(buttonPin,HIGH);
+  pinMode(buttonPin,INPUT);
+}
+
+void sevenSegWrite(byte digit) {
+  byte pin = 2;
+  for (byte seg = 0; seg < 7; ++seg) {
+    digitalWrite(pin, seven_seg_digits[digit][seg]);
+    ++pin;
+  }
+}
+
+void loop() {
+  
+  buttonState = digitalRead(buttonPin);
+  
+  if(buttonState == HIGH){
+    num ++;
+    if(num > 9){
+       num = 0;
+    }
+}
+  sevenSegWrite(num); 
+ Serial.println(num);  
+ delay(500);
+
+}
+  
